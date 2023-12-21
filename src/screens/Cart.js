7fd3acf8899fd96624dee0163b3d8346c73597f8
kavyansh-11 <kavyanshgoswami1109@gmail.com>
2023-12-22 00:02:20 +0530

    pushing project

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -63,7 +63,7 @@ export default function Cart() {
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
                 <td>{food.price}</td>
-                <td ><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td>
+                <td ><button type="button" className="btn p-0 bg-success"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td>
                 </tr>
             ))}
           </tbody>
